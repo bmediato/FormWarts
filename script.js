be6a9efedf0dc8e7353f6036f7e8f5ae888d44e0
heidegomes <heidegc@hotmail.com>
2022-08-11 17:54:45 -0300

    requisito 18 feito, aguardando requisitos anteriores para testar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Requisito 3 - Botão que valida o formulário
 function validar() {
   const email = document.getElementById('input-email');
   const senha = document.getElementById('input-senha');
@@ -11,9 +12,24 @@ function validar() {
   } else {
     alert("Email ou senha inválidos.");
   }
-
 }
 
 const button = document.getElementsByTagName('button')[0];
 console.log(button);
 button.addEventListener('click', validar);
+//Fim Requisito 3
+
+//Requisito 18 - Crie um botão de "Enviar" para submeter o formulário.
+const buttonSend = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+
+buttonSend.disabled = false;
+input.addEventListener("change", stateHandle);
+function stateHandle() {
+  if (checkbox.value === "") {
+    button.disabled = false; 
+  } else {
+    button.disabled = true;
+  }
+}
+//Fim Requisito 18 - fonte: https://www.delftstack.com/pt/howto/javascript/javascript-disable-button/  ,  https://www.w3schools.com/jsref/prop_select_disabled.asp
