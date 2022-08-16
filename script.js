958742b62ab19e6786908f7cf49c9d5db31af208
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-16 20:49:23 -0300

    ajeitando erros de lint

--- script.js
+++ script.js
@@ -49,19 +49,17 @@ checkbox.addEventListener('change', stateHandle);
 
 // Fim Requisito 18 - fonte: https://www.delftstack.com/pt/howto/javascript/javascript-disable-button/  ,  https://www.w3schools.com/jsref/prop_select_disabled.asp
 
+// Requisito 21
 
-//Requisito 21
+// const formPrint = document.getElementById('form-data');
 
-let formPrint = document.getElementById('form-data')
+function viewForm() {
+  const viewInput = document.getElementById('form-data');
 
-
-function viewForm (){
-  let viewInput = document.getElementById('form-data');
-
-  let nomeInput = document.getElementById('input-name');
-  let sobrenomeInput = document.getElementById('input-lastname');
-  let emailInput = document.getElementById('email');
-  let casaInput = document.getElementById('house');
+  const nomeInput = document.getElementById('input-name');
+  const sobrenomeInput = document.getElementById('input-lastname');
+  const emailInput = document.getElementById('email');
+  const casaInput = document.getElementById('house');
   // let familiaInput = document.getElementById('label-family');
   // let materiaInput = document.getElementById('container-conteudo');
   // let avaliacaoInput = document.getElementById('n-avaliacao');
@@ -74,7 +72,6 @@ function viewForm (){
   // Materia: ${materiaInput.value};
   // Avaliação: ${avaliacaoInput.value};
   // Observações: ${observacoesInput.value};
-
 }
 
 // buttonSend.addEventListener('click', function(event){
